cb2f53f45324c30028fe98fb4a0b800d2bd01154
devincody <devincody@gmail.com>
2018-04-10 20:47:52 -0700

    corrected gpu checks

--- lab1/src/blur.cu
+++ lab1/src/blur.cu
@@ -12,6 +12,25 @@
 
 #include "cuda_header.cuh"
 
+
+/*
+ * NOTE: You can use this macro to easily check cuda error codes 
+ * and get more information. 
+ * 
+ * Modified from:
+ * http://stackoverflow.com/questions/14038589/
+ *   what-is-the-canonical-way-to-check-for-errors-using-the-cuda-runtime-api
+ */
+
+inline void gpu_assert(cudaError_t code, const char *file, int line,
+                       bool abort = true) {
+    if (code != cudaSuccess) {
+        fprintf(stderr, "gpu_assert: %s %s %d\n",
+                cudaGetErrorString(code), file, line);
+        exit(code);
+    }
+}
+
 #define gpu_errchk(ans) { gpu_assert((ans), __FILE__, __LINE__); }
 
 CUDA_CALLABLE
