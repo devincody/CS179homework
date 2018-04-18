47a5c610bb5a4b4ffa7361186fb9e67ee11ecd93
devincody <devincody@gmail.com>
2018-04-18 01:14:08 -0700

    lab2

--- lab2/transpose_device.cu
+++ lab2/transpose_device.cu
@@ -76,8 +76,8 @@ void shmemTransposeKernel(const float *input, float *output, int n) {
     __syncthreads();
 
 
-    i =     threadIdx.x + 64 * blockIdx.x; //global indicies
-    j = 4 * threadIdx.y + 64 * blockIdx.y;
+    i =     threadIdx.x + 64 * blockIdx.y; //global indicies
+    j = 4 * threadIdx.y + 64 * blockIdx.x;
     end_j = j + 4;
 
     ii =  4*threadIdx.y;
